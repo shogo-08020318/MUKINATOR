3e1187540e1d14e270821d81dd08d00fcbc4dfdd
Shogo <sho5tkmr0802@gmail.com>
2021-06-12 13:10:57 +0900

    Fix: サンプルデータ(仮)作成

--- app/models/trouble.rb
+++ app/models/trouble.rb
@@ -1,4 +1,6 @@
 class Trouble < ApplicationRecord
   validates :name, presence: true
   validates :kind, presence: true
+
+  enum kind: { health: 0, ambition: 1, relationship: 2, money: 3 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 悩み
+Trouble.create(
+  [
+    # 健康
+    { name: '痩せない',          kind: 0 },
+    { name: '肌荒れ',            kind: 0 },
+    # 将来
+    { name: '作業に集中できない',  kind: 1 },
+    { name: '少子化が不安',       kind: 1 },
+    # 人間関係
+    { name: '初対面で話せない',    kind: 2 },
+    { name: '上司が怖い',         kind: 2 },
+    # お金
+    { name: 'お金がない',         kind: 3 },
+    { name: '給料が上がらない',    kind: 3 }
+  ]
+)
+
+
+# 解決策の理由
+Reason.create(
+  [
+    { name: '痩せないを選択しました',         trouble_id: 1 },
+    { name: '肌荒れを選択しました',           trouble_id: 2 },
+    { name: '作業の集中できないを選択しました', trouble_id: 3 },
+    { name: '少子化が不安を選択しました',      trouble_id: 4 },
+    { name: '初対面で話せないを選択しました',   trouble_id: 5 },
+    { name: '上司が怖いを選択しました',        trouble_id: 6 },
+    { name: 'お金がないを選択しました',        trouble_id: 7 },
+    { name: '給料が上がらないを選択しました',   trouble_id: 8 }
+  ]
+)
+
