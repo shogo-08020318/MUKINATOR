3cf407d75a296ad4081281e048cefa9a7ada8bfd
Shogo <sho5tkmr0802@gmail.com>
2021-08-09 23:25:58 +0900

    Fix: 未入力の場合の対処

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -8,38 +8,54 @@ class ResultsController < ApplicationController
   def trouble_category
     # ニックネームと性別をセッションに一時保持
     # binding.pry
-    session[:nickname] = params[:result][:nickname]
+    if params[:nickname].present?
+      # binding.pry
+      session[:nickname] = params[:nickname]
     # session[:gender] = params[:result][:gender]
     # 悩みのカテゴリー選択ページ
     # binding.pry
-    render 'result/trouble_category'
+      render 'result/trouble_category'
+    else
+      render 'result/name_gender'
+    end
   end
 
   def trouble_select
     # 選択したカテゴリーを使ってそのカテゴリーの悩みを取得
     # binding.pry
-    @troubles = Trouble.where(kind: params[:kind])
+    if params[:kind].present?
+      @troubles = Trouble.where(kind: params[:kind])
     # 悩みの選択ページ
     # binding.pry
-    render 'result/trouble_select'
+      render 'result/trouble_select'
+    else
+      render 'result/trouble_category'
+    end
   end
 
   def create
     # 選択した悩みに対する解決策の理由を取得
     # binding.pry
-    session[:reason] = Reason.find(params[:trouble_id]).id
-    # session[:reason] = @reason.id
-    # binding.pry
-    # @result = Result.new(name: session[:nickname], gender: session[:gender], reason_id: session[:reason])
-    @result = Result.new(name: session[:nickname], reason_id: session[:reason])
-    # @result = Result.new(result_params)
-    # binding.pry
-    @result.save!
-    @video = []
-    @video[0] = Exercise.where(category: 'man').sample
-    @video[1] = Exercise.where(category: 'woman').sample
-    @video[2] = Exercise.where(category: 'other').sample
-    render 'result/result'
+    unless params[:trouble_id].nil?
+      # binding.pry
+      session[:trouble_id]
+      session[:reason] = Reason.find(session[:trouble_id]).id
+      # session[:reason] = @reason.id
+      # binding.pry
+      # @result = Result.new(name: session[:nickname], gender: session[:gender], reason_id: session[:reason])
+      @result = Result.new(name: session[:nickname], reason_id: session[:reason])
+      # @result = Result.new(result_params)
+      # binding.pry
+      @result.save!
+      @video = []
+      @video[0] = Exercise.where(category: 'man').sample
+      @video[1] = Exercise.where(category: 'woman').sample
+      @video[2] = Exercise.where(category: 'other').sample
+      render 'result/result'
+    else
+      # binding.pry
+      render 'result/trouble_select'
+    end
   end
 
   private
