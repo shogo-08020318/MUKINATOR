a05267fece901b29203fe95e768d8c0b33ecf593
Shogo <sho5tkmr0802@gmail.com>
2021-08-11 11:25:27 +0900

    Fix: 未入力・未選択に対応

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,59 +2,52 @@ class ResultsController < ApplicationController
   def new
     # 「相談を始める」を押下、ニックネーム入力ページ
     @result = Result.new
-    render 'result/name_gender'
+    render 'results/nickname'
   end
 
   def trouble_category
     # ニックネームと性別をセッションに一時保持
-    # binding.pry
     if params[:nickname].present?
-      # binding.pry
       session[:nickname] = params[:nickname]
     # session[:gender] = params[:result][:gender]
     # 悩みのカテゴリー選択ページ
-    # binding.pry
-      render 'result/trouble_category'
+      render 'results/trouble_category'
     else
-      render 'result/name_gender'
+      render 'results/nickname'
     end
   end
 
   def trouble_select
     # 選択したカテゴリーを使ってそのカテゴリーの悩みを取得
-    # binding.pry
     if params[:kind].present?
-      @troubles = Trouble.where(kind: params[:kind])
+      session[:kind] = params[:kind]
+      @troubles = Trouble.where(kind: session[:kind])
     # 悩みの選択ページ
-    # binding.pry
-      render 'result/trouble_select'
+      render 'results/trouble_select'
     else
-      render 'result/trouble_category'
+      render 'results/trouble_category'
     end
   end
 
   def create
     # 選択した悩みに対する解決策の理由を取得
-    # binding.pry
-    unless params[:trouble_id].nil?
-      # binding.pry
-      session[:trouble_id]
+    if params[:trouble_id].present?
+      session[:trouble_id] = params[:trouble_id]
       session[:reason] = Reason.find(session[:trouble_id]).id
       # session[:reason] = @reason.id
-      # binding.pry
       # @result = Result.new(name: session[:nickname], gender: session[:gender], reason_id: session[:reason])
       @result = Result.new(name: session[:nickname], reason_id: session[:reason])
       # @result = Result.new(result_params)
-      # binding.pry
       @result.save!
       @video = []
       @video[0] = Exercise.where(category: 'man').sample
       @video[1] = Exercise.where(category: 'woman').sample
       @video[2] = Exercise.where(category: 'other').sample
-      render 'result/result'
+      render 'results/result'
     else
-      # binding.pry
-      render 'result/trouble_select'
+      # ここで再度悩みを取得しないと、レンダリング先で@troublesがnilになる
+      @troubles = Trouble.where(kind: session[:kind])
+      render 'results/trouble_select'
     end
   end
 
