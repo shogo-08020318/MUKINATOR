c5cc4506ecb8e83341dc4e1a97c7101f130fe55d
Shogo <sho5tkmr0802@gmail.com>
2021-10-06 14:31:13 +0900

    Add: ヘッダー作成

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $(".navbar-burger").click(function() {
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+    $(".navbar-brand").toggleClass("navbar-color");
+    $(".navbar-end .navbar-item").toggleClass("navbar-burger-menu");
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
   });
 });
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 require('jquery')
+require('header.js')
 require('wise_saying.js')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
