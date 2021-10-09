1930009f9c3053e7d08e18d21d05ba3386cf7202
Shogo <sho5tkmr0802@gmail.com>
2021-10-09 09:11:01 +0900

    Fix: 表示時間の間隔を短縮

--- app/javascript/results.js
+++ app/javascript/results.js
@@ -8,40 +8,40 @@ $(function() {
   });
 
   // 悩みの表示
-  $('#trouble-name').delay(11000).queue(function(){
+  $('#trouble-name').delay(10500).queue(function(){
     $(this).addClass('is-opacity-active');
   });
 
   // 「解決方法は、」の表示
-  $('#result-text').delay(13000).queue(function(){
+  $('#result-text').delay(11500).queue(function(){
     $(this).addClass('is-opacity-active');
   });
 
   // 「筋トレ」の表示
-  $('#solution').delay(16000).queue(function(){
+  $('#solution').delay(12500).queue(function(){
     $(this).addClass('is-opacity-active');
   });
 
   // 理由の表示
-  $('#result-reason').delay(18000).queue(function(){
+  $('#result-reason').delay(13500).queue(function(){
     $(this).addClass('is-opacity-active');
   });
 
   // 画像
-  $('#result-img').delay(20000).queue(function(){
+  $('#result-img').delay(14000).queue(function(){
     $(this).addClass('is-opacity-active');
   });
 
   // 名言エリア
-  $('#wise-saying-label').delay(21000).queue(function(){
+  $('#wise-saying-label').delay(15000).queue(function(){
     $(this).addClass('is-opacity-active');
   });
 
-  $('#wise-saying').delay(23000).queue(function(){
+  $('#wise-saying').delay(15500).queue(function(){
     $(this).addClass('is-opacity-active');
   });
 
-  $('#syawing-person').delay(24000).queue(function(){
+  $('#syawing-person').delay(16000).queue(function(){
     $(this).addClass('is-opacity-active');
   });
 });
