b2fa59d5ba09e6bd56ac61d9758aeaee7d30ca2f
Shogo <sho5tkmr0802@gmail.com>
2021-10-18 16:26:46 +0900

    Fix: titleカラムの削除に伴いバリデーション削除

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,5 +1,5 @@
 class Exercise < ApplicationRecord
-  validates :title, presence: true
+  # validates :title, presence: true
   validates :video, presence: true
   validates :category, presence: true
 end
