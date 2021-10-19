4d6e4f1f4d12cdba4bb34e2e2b9791be3e0da070
Shogo <sho5tkmr0802@gmail.com>
2021-10-19 17:47:45 +0900

    Add: meta_tagsの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,34 @@ module ApplicationHelper
 
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
+
+  def default_meta_tags
+    {
+      site: 'MUKINATOR',
+      title: '悩みを筋トレで解決！',
+      reverse: true,
+      charset: 'utf-8',
+      description: '日常生活で生まれる悩みや不安を全て筋トレで解決するアプリ',
+      keywords: '筋肉, 筋トレ, ボディメイク, 悩み, 不安',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('favicon.png') },
+        { href: image_url('favicon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: site,
+        title: title,
+        description: description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@takemuu_pote',
+      }
+    }
+  end
 end
