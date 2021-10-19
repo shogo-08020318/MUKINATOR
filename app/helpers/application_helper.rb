7af4b18d9cb5b3dd895fdc574425a42776c948c4
Shogo <sho5tkmr0802@gmail.com>
2021-10-19 21:21:00 +0900

    Fix: 動画の部分非表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
       charset: 'utf-8',
       description: '日常生活で生まれる悩みや不安を全て筋トレで解決するアプリ',
       keywords: '筋肉, 筋トレ, ボディメイク, 悩み, 不安',
-      canonical: 'https://mukinator.herokuapp.com/',
+      canonical: request.original_url,
       separator: '|',
       icon: [
         { href: image_url('favicon.png') },
@@ -24,7 +24,7 @@ module ApplicationHelper
         title: :title,
         description: :description,
         type: 'website',
-        url: 'https://mukinator.herokuapp.com/',
+        url: request.original_url,
         image: image_url('ogp.png'),
         locale: 'ja_JP'
       },
