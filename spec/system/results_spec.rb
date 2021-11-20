977799ed412272766e3eef0291e831a863ffaa3c
Shogo <sho5tkmr0802@gmail.com>
2021-11-20 23:56:35 +0900

    Fix: トップページのテスト作成

--- spec/system/results_spec.rb
+++ spec/system/results_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Results", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  describe 'ニックネームから解決策提示' do
+    context 'トップページ' do
+      it 'トップページにアクセスして正常に表示されること' do
+        visit root_path
+        expect(page).to have_content('この世は筋肉が全て！')
+      end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+      it '「相談を始める」ボタンを押下するとニックネーム入力ページに遷移すること' do
+        visit root_path
+        click_link '相談を始める'
+        expect(current_path).to eq(new_result_path)
+        expect(page).to have_content('ニックネームを入力してください')
+      end
+    end
+  end
 end
