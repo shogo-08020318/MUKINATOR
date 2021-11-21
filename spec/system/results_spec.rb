2131a83347e1f885f7b174034a07b5038375257a
Shogo <sho5tkmr0802@gmail.com>
2021-11-21 11:39:25 +0900

    Fix: 悩みのカテゴリーのテスト作成

--- spec/system/results_spec.rb
+++ spec/system/results_spec.rb
@@ -34,5 +34,71 @@ RSpec.describe "Results", type: :system do
         expect(page).to have_content('ニックネームを入力してください。')
       end
     end
+
+    context '悩みカテゴリー選択ページ' do
+      before do
+        visit new_result_path
+        fill_in 'result_nickname', with: 'テスト太郎'
+        click_button '次へ'
+      end
+
+      it '悩みカテゴリーのラジオボタンが存在すること' do
+        expect(current_path).to eq(trouble_category_results_path)
+        has_unchecked_field?('kind_health')
+        has_unchecked_field?('kind_ambition')
+        has_unchecked_field?('kind_relationship')
+        has_unchecked_field?('kind_money')
+      end
+
+      it '「健康」を選択した場合、「健康」の悩みが表示されること' do
+        has_unchecked_field?('kind_health')
+        choose('kind_health')
+        has_checked_field?('kind_health')
+        click_button '次へ'
+        expect(current_path).to eq(trouble_select_results_path)
+        expect(page).to have_content('悩みを選択してください')
+        has_unchecked_field?('ハゲるのが怖い')
+        has_unchecked_field?('痩せない')
+      end
+
+      it '「キャリア・夢」を選択した場合、「キャリア・夢」の悩みが表示されること' do
+        has_unchecked_field?('kind_ambition')
+        choose('kind_ambition')
+        has_checked_field?('kind_ambition')
+        click_button '次へ'
+        expect(current_path).to eq(trouble_select_results_path)
+        expect(page).to have_content('悩みを選択してください')
+        has_unchecked_field?('就職・転職できるか不安')
+        has_unchecked_field?('仕事を失うかもしれない')
+      end
+
+      it '「人間関係」を選択した場合、「人間関係」の悩みが表示されること' do
+        has_unchecked_field?('kind_relationship')
+        choose('kind_relationship')
+        has_checked_field?('kind_relationship')
+        click_button '次へ'
+        expect(current_path).to eq(trouble_select_results_path)
+        expect(page).to have_content('悩みを選択してください')
+        has_unchecked_field?('初対面で話せない')
+        has_unchecked_field?('上司が怖い')
+      end
+
+      it '「お金」を選択した場合、「お金」の悩みが表示されること' do
+        has_unchecked_field?('kind_money')
+        choose('kind_money')
+        has_checked_field?('kind_money')
+        click_button '次へ'
+        expect(current_path).to eq(trouble_select_results_path)
+        expect(page).to have_content('悩みを選択してください')
+        has_unchecked_field?('浪費してしまう')
+        has_unchecked_field?('貯金ができない')
+      end
+
+      it '悩みのカテゴリーが未選択の場合、ページが再表示されること' do
+        expect(current_path).to eq(trouble_category_results_path)
+        click_button '次へ'
+        expect(page).to have_content('悩みのカテゴリーを選択してください。')
+      end
+    end
   end
 end
