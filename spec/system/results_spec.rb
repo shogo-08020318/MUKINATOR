4b757e60f9c9136de3c7136d121528e4714e24d4
Shogo <sho5tkmr0802@gmail.com>
2021-11-21 11:37:42 +0900

    Fix: ニックネーム入力のテスト作成

--- spec/system/results_spec.rb
+++ spec/system/results_spec.rb
@@ -15,5 +15,24 @@ RSpec.describe "Results", type: :system do
         expect(page).to have_content('ニックネームを入力してください')
       end
     end
+
+    context 'ニックネーム入力ページ' do
+      it 'ニックネームを入力して「次へ」ボタンを押下するとページ遷移すること' do
+        visit new_result_path
+        expect(page).to have_content('ニックネームを入力してください')
+        fill_in 'result_nickname', with: 'テスト太郎'
+        click_button '次へ'
+        expect(current_path).to eq(trouble_category_results_path)
+        expect(page).to have_content('悩みのカテゴリー')
+        expect(page).to have_content('悩みのカテゴリーを選択してください')
+      end
+
+      it 'ニックネーム未入力の場合、ページが再表示されること' do
+        visit new_result_path
+        fill_in 'result_nickname', with: ''
+        click_button '次へ'
+        expect(page).to have_content('ニックネームを入力してください。')
+      end
+    end
   end
 end
